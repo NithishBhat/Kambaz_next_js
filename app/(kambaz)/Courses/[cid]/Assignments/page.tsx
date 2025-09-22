ed82ad8f2c572a76060ebb7c789716bc025d06f4
Nithish Bhat <nithishbhat1998@gmail.com>
2025-09-22 01:57:34 -0400

    Correction
    <Link was imported>

--- app/(kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,3 +1,5 @@
+
+import Link from "next/link";
 export default function Assignments() {
   return (
     <div id="wd-assignments">
@@ -9,28 +11,28 @@ export default function Assignments() {
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <link href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A1 - CSS + HTML
-          </link>
+          </Link>
           <br/>
           Mutliple Modules | <strong>Not Available Until</strong> May 6 at 12:00am | <br/>
           <strong> Due</strong> May 7 at 12:00am | 100 pts
         </li>
           <li className="wd-assignment-list-item">
-          <link href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A2 - ENV + HTML
-          </link> 
+          </Link> 
           <br/>
           Mutliple Modules | <strong>Not Available Until</strong> May 6 at 12:00am | <br/>
           <strong> Due</strong> May 7 at 12:00am | 100 pts
           </li>
             <li className="wd-assignment-list-item">
-            <link href="/Courses/1234/Assignments/123"
+            <Link href="/Courses/1234/Assignments/123"
               className="wd-assignment-link" >
               A3 - REACT + HTML
-            </link> 
+            </Link> 
             <br/>
             Mutliple Modules | <strong>Not Available Until</strong> May 6 at 12:00am | <br/>
             <strong> Due</strong> May 7 at 12:00am | 100 pts
