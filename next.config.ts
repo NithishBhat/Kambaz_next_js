c80f0a119b596dafa6987835d35c387d79a357c5
Nithish Bhat <nithishbhat1998@gmail.com>
2025-10-21 21:08:07 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
+/* eslint-disable */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    
-    ignoreDuringBuilds: true,
-  },
-
+  /* config options here */
 };
 
 export default nextConfig;
-
