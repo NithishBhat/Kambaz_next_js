6acf10088419a3a2f6cfe1111ca5cbdfadfc5929
Nithish Bhat <nithishbhat1998@gmail.com>
2025-10-21 20:49:43 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  // If you're using images from external domains, add them here
-  images: {
-    domains: [],
-  },
-}
 
-module.exports = nextConfig
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+module.exports = { eslint: { ignoreDuringBuilds: true, }, }
