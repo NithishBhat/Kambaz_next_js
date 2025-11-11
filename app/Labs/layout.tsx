f77536f7e64d5945f33179a5f0340b6b67e7ce11
NithishBhat <nithishbhat1998@gmail.com>
2025-11-11 01:21:02 -0500

    added bread crumb

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,20 +1,26 @@
+"use client"; 
+
 import { ReactNode } from "react";
 import TOC from "./TOC";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
+import store from "./Lab4/store"; 
+import { Provider } from "react-redux"; 
 export default function LabsLayout({
- children,
+  children,
 }: Readonly<{ children: ReactNode }>) {
- return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="100px">
-           <TOC />
-         </td>
-         <td valign="top">{children}</td>
-       </tr>
-     </tbody>
-   </table>
-);}
+  return (
+    <Provider store={store}>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="100px">
+              <TOC />
+            </td>
+            <td valign="top">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
+  );
+}
