b1218587f6f49b5e0512b043bfe3d400fab69424
NithishBhat <nithishbhat1998@gmail.com>
2025-12-13 12:21:34 -0500

    adding path parameters

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -7,7 +7,9 @@ import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+
 export default function Lab5() {
   return (
     <div id="wd-lab5">
@@ -16,12 +18,16 @@ export default function Lab5() {
         <a href={`${HTTP_SERVER}/lab5/welcome`} className="list-group-item">
           Welcome
         </a>
-      </div><hr />
+      </div>
+      <hr />
       <EnvironmentVariables />
-      <QueryParameters/>
-      <WorkingWithObjects/>
-      <WorkingWithArrays/>
-      <HttpClient/>
-      <WorkingWithArraysAsynchronously/>
-      <WorkingWithObjectsAsynchronously/>
-    </div>)};
+      <PathParameters />
+      <QueryParameters />
+      <WorkingWithObjects />
+      <WorkingWithArrays />
+      <HttpClient />
+      <WorkingWithArraysAsynchronously />
+      <WorkingWithObjectsAsynchronously />
+    </div>
+  );
+}
